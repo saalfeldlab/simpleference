57f748d4c6a8679ae1be8df85284d43afe42cd25
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2017-11-14 18:32:47 -0500

    complete inference for fib25 case

--- experiments/fib25_dist/complete_inference.py
+++ experiments/fib25_dist/complete_inference.py
@@ -3,35 +3,37 @@ import os
 from concurrent.futures import ProcessPoolExecutor
 from subprocess import call
 from simpleference.inference.util import get_offset_lists
+from stitch import stitch
 
 
-def single_inference(sample, gpu, iteration):
-    call(['./run_inference.sh', sample, str(gpu), str(iteration)])
+def single_inference(gpu, iteration):
+    call(['./run_inference.sh', str(gpu), str(iteration)])
     return True
 
 
-def complete_inference(sample, gpu_list, iteration):
+def complete_inference(gpu_list, iteration):
 
     # path to the raw data
-    raw_path = '/groups/saalfeld/home/papec/Work/neurodata_hdd/cremi_warped/cremi_warped_sample%s.h5' % sample
 
+    raw_path = '/groups/saalfeld/saalfeldlab/larissa/data/fib25/grayscale_sub.h5'
     # make the offset files, that assign blocks to gpus
-    save_folder = './offsets_sample%s' % sample
-    get_offset_lists(raw_path, gpu_list, save_folder)
+    save_folder = '/nrs/saalfeld/heinrichl/segmentation/distance_thirdtest/offsets'
+    get_offset_lists(raw_path, gpu_list, save_folder, output_shape=(44,)*3, randomize=True)
 
     # run multiprocessed inference
     with ProcessPoolExecutor(max_workers=len(gpu_list)) as pp:
-        tasks = [pp.submit(single_inference, sample, gpu, iteration) for gpu in gpu_list]
+        tasks = [pp.submit(single_inference, gpu, iteration) for gpu in gpu_list]
         result = [t.result() for t in tasks]
 
     if all(result):
         print("All gpu's finished inference properly.")
+        t_stitch = stitch()
+        print("Stitching in {0:}".format(t_stitch))
     else:
         print("WARNING: at least one process didn't finish properly.")
 
-
 if __name__ == '__main__':
-    gpu_list = [0, 2, 3, 4, 5, 6, 7]
-    iteration = 100000
-    for sample in ('B+', 'C+'):
-        complete_inference(sample, gpu_list, iteration)
+    gpu_list = [0, 1, 2, 3, 4, 5, 6, 7]
+    iteration = 400000
+    complete_inference(gpu_list, iteration)
+
