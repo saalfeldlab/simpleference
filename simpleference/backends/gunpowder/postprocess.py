44d0556c5c4d909f7e4daf884a2dd085c2c77313
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2018-03-07 13:31:46 -0500

    add postprocess: threshold + connected component

--- simpleference/backends/gunpowder/postprocess.py
+++ simpleference/backends/gunpowder/postprocess.py
@@ -1,12 +1,18 @@
+from __future__ import print_function
 import numpy as np
 import scipy.ndimage
 
 
-def threshold_cc(data, output_bounding_box, thr=0.):
+def threshold_cc(data, output_bounding_box, thr=0., ds_shape=(1, 1, 1), output_shape=(1, 1, 1)):
     if not isinstance(data, np.ndarray):
         data = np.array(data)
     output = (data > thr).astype(np.uint64)
     scipy.ndimage.label(output, output=output)
+    block_offset = [int(output_bounding_box[k].start // output_shape[k]) for k in range(len(output_shape))]
+    block_shape = [int(np.ceil(s/float(o))) for s, o in zip(ds_shape, output_shape)]
+    print("Processing block at:", block_offset)
+    id_offset = np.ravel_multi_index(block_offset, block_shape)*np.prod(output_shape)
+    output[output > 0] += id_offset
     return [data.squeeze(), output.squeeze()]
 
 
