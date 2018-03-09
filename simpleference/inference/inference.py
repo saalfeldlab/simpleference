8eba11650500a0dce35fb9588dfa0b0c9407eccc
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-03-09 17:44:46 -0500

    Start implementation of inference based on butler.BlockService

--- simpleference/inference/inference.py
+++ simpleference/inference/inference.py
@@ -6,6 +6,7 @@ import numpy as np
 import dask
 import toolz as tz
 import functools
+from butler.block_service import BlockClient
 
 from .io import IoN5  # , IoDVID, IoHDF5
 
@@ -47,7 +48,7 @@ def run_inference_n5(prediction,
                      postprocess,
                      raw_path,
                      save_file,
-                     offset_list,
+                     server_address,
                      input_shape,
                      output_shape,
                      input_key,
@@ -70,7 +71,7 @@ def run_inference_n5(prediction,
 
     io_out = IoN5(save_file, target_keys, channel_order=channel_order)
     run_inference(prediction, preprocess, postprocess, io_in, io_out,
-                  offset_list, input_shape, output_shape, padding_mode=padding_mode,
+                  server_address, input_shape, output_shape, padding_mode=padding_mode,
                   num_cpus=num_cpus, log_processed=log_processed)
     # This is not necessary for n5 datasets
     # which do not need to be closed, but we leave it here for
@@ -84,7 +85,7 @@ def run_inference(prediction,
                   postprocess,
                   io_in,
                   io_out,
-                  offset_list,
+                  server_address,
                   input_shape,
                   output_shape,
                   padding_mode='reflect',
@@ -95,9 +96,8 @@ def run_inference(prediction,
     assert callable(preprocess)
     assert len(output_shape) == len(input_shape)
 
-    n_blocks = len(offset_list)
-    print("Starting prediction...")
-    print("For %i number of blocks" % n_blocks)
+    print("Starting prediction with client to BlockService %s:%i" % (server_address))
+    client = BlockClient(server_address[0], server_address[1])
 
     # the additional context requested in the input
     context = np.array([input_shape[i] - output_shape[i]
@@ -106,12 +106,29 @@ def run_inference(prediction,
 
     shape = io_in.shape
 
+    # TODO we need to handle the case `None` (which means no more blocks)
+    # Option 1: switch to futures ?!
+    # Option 2: provide second code stream, which just passes through `None and does nothing`
+    # -> 2 should be easier !
     @dask.delayed
+    def get_offset():
+        return client.request()
+
+    @dask.delayed(nout=2)
     def load_offset(offset):
-        return load_input(io_in, offset, context, output_shape,
-                          padding_mode=padding_mode)
-    preprocess = dask.delayed(preprocess)
-    predict = dask.delayed(prediction)
+        return (load_input(io_in, offset, context, output_shape,
+                           padding_mode=padding_mode),
+                offset)
+
+    @dask.delayed(nout=2)
+    def prepro(data, offset):
+        return preprocess(data), offset
+
+    @dask.delayed
+    def predict(data, offset):
+        pred = prediction(data)
+        client.request(offset)
+        return pred
 
     if postprocess is not None:
         postprocess = dask.delayed(postprocess)
@@ -137,16 +154,19 @@ def run_inference(prediction,
         return 1
 
     @dask.delayed
-    def log(off):
+    def log(offset):
         if log_processed is not None:
             with open(log_processed, 'a') as log_f:
-                log_f.write(json.dumps(off) + ', ')
-        return off
+                log_f.write(json.dumps(offset) + ', ')
+        return offset
 
     # iterate over all the offsets, get the input data and predict
     results = []
-    for offset in offset_list:
-        output = tz.pipe(offset, log, load_offset, preprocess, predict)
+    # TODO get max-number of blocks from client
+    # or can we somehow get this into a while loop ???
+    max_num_requests = 100000
+    for _ in range(max_num_requests):
+        output, offset = tz.pipe(get_offset, log, load_offset, prepro, predict)
         output_crop, output_bounding_box = verify_shape(offset, output)
         if postprocess is not None:
             output_crop = postprocess(output_crop, output_bounding_box)
